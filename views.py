8703cf8454da652136e0a63a26a13b472114cdec
Aditya-Saini <aju180601@gmail.com>
2021-01-03 21:18:03 +0530

    checkecking for options and incrementing marks

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 import pandas as pd
@@ -9,12 +9,22 @@ data['Questions']=data['Questions'].astype(str)
 questions=data['Questions']
 answers=data.drop(['Questions'],axis=1).values
 dList={'ques' : questions, 'ans':answers}
-
+print(answers[0][0])
 def home(request):
     
     return render(request, 'QuizManagement/home.html')
 
 def test(request):
+    marks=0
+    if request.method == "POST":
+        for i in range(1,11):
+            myVar = request.POST.get("quiz-"+str(i))
+            print(myVar,i,answers[i-1][3])
+            if(str(myVar) == str(answers[i-1][0])):
+                marks+=1
+        print(marks)
+        return redirect("home")
+            
     content={'fin' : dList }
     return render(request, 'QuizManagement/test.html', content)
 
